503ba5a04f3fe657cebbe9610e1a2d29fae01f65
ramishka-zettab <ramishka.madhushan@zettab.io>
2026-06-03 22:41:21 +0530

    Group Vercel API route functions

--- api/[...path].js
+++
@@ -1,7 +0,0 @@
-const handleExpress = require("./_handler");
-
-module.exports = async function handler(req, res) {
-  const path = req.query?.path;
-  const segments = Array.isArray(path) ? path : path ? [path] : [];
-  return handleExpress(req, res, `/api/${segments.join("/")}`);
-};
