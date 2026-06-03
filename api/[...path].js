d8b884b7b8f1a32ac1a97dbb6eda2253d0b13969
ramishka-zettab <ramishka.madhushan@zettab.io>
2026-06-03 21:12:49 +0530

    Normalize Vercel API paths

--- api/[...path].js
+++ api/[...path].js
@@ -3,5 +3,8 @@ let appPromise;
 module.exports = async function handler(req, res) {
   appPromise ??= import("../backend/dist/src/app.js").then(({ createApp }) => createApp());
   const app = await appPromise;
+  if (req.url && !req.url.startsWith("/api")) {
+    req.url = `/api${req.url}`;
+  }
   return app(req, res);
 };
